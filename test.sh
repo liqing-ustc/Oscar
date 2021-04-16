18782252ac81824d370203474fcf4771e1191d20
Qing Li <dylan.liqing@gmail.com>
2021-04-16 15:06:46 -0700

    Update

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ python oscar/run_captioning.py \
     --do_eval \
     --test_yaml test.yaml \
     --per_gpu_eval_batch_size 64 \
-    --num_beams 5 \ 
+    --num_beams 5 \
     --max_gen_length 20 \
     --eval_model_dir data/output/oscar.20210415/checkpoint-59-66420/ # could be base or large models
