9eadb7b79979d7b23ba930de8f991a35c8206854
Qing Li <dylan.liqing@gmail.com>
2021-05-20 12:08:38 -0700

    add batch submit

--- submit_batch_caption.py
+++ submit_batch_caption.py
@@ -13,38 +13,6 @@ if not stamp:
 
 submit_cmd = "python -m aml_tools.aml_submit --input_dir . --output_dir {} --num_nodes 1 --exp_name t-lqing " \
              "--config_yaml ~/.azureml/{}.yaml --cmd \"{}\""
-# job_cmd = "oscar/run_captioning.py \
-#     --model_name_or_path models/pretrained_base/checkpoint-2000000 \
-#     --do_train \
-#     --do_test \
-#     --evaluate_during_training --save_steps 1000 \
-#     --do_lower_case \
-#     --add_od_labels \
-#     --learning_rate 3e-5 \
-#     --per_gpu_train_batch_size 64 \
-#     --num_train_epochs 60 \
-#     --tie_weights \
-#     --freeze_embedding \
-#     --label_smoothing 0.1 \
-#     --drop_worst_ratio 0.2 \
-#     --drop_worst_after 20000 \
-#     --output_dir output/ \
-#     --self_slimming --inter_slimming --l1_loss_self_coef=1e-4 --l1_loss_inter_coef=1e-4 \
-# "
-
-# job_cmd = "python oscar/run_vqa.py \
-#     --data_label_type mask --img_feature_type faster_r-cnn \
-#     --task_name vqa_text --data_dir datasets/vqa --txt_data_dir datasets/vqa \
-#     --label_file datasets/vqa/trainval_ans2label.pkl \
-#     --model_type bert --model_name_or_path models/pretrained_base/checkpoint-2000000 \
-#     --do_train --do_lower_case --max_seq_length 128 \
-#     --per_gpu_eval_batch_size 256 --per_gpu_train_batch_size 32 \
-#     --learning_rate 5e-05 --num_train_epochs 25 \
-#     --save_epoch 1 --seed 88 --evaluate_during_training --logging_steps 4000 --drop_out 0.3 \
-#     --weight_decay 0.05 --warmup_steps 0 --loss_type bce --img_feat_format pt \
-#     --classifier linear --cls_hidden_scale 3 \
-#     --output_dir output/ \
-# "
 
 output_dir = 't-lqing/experiments/captioning/pruning_{}_{}/{}'
 job_cmd = "oscar/run_captioning_pruning.py \
---
+++ submit_batch_vqa.py
@@ -0,0 +1,36 @@
+#! /home/qing/.virtualenvs/azure/bin/python
+import os
+import pdb
+from datetime import datetime
+
+ws_config = 'itp_acv' 
+# ws_config = 'vlp_cust' 
+# ws_config = 'vlp' 
+stamp = ''
+
+if not stamp:
+    stamp = datetime.now().strftime('%Y%m%d.%H%M%S')
+
+task = 'vqa'
+submit_cmd = "python -m aml_tools.aml_submit --input_dir . --output_dir {} --num_nodes 1 --exp_name liqing-{} " \
+             "--config_yaml ~/.azureml/{}.yaml --cmd \"{}\""
+
+output_dir = 't-lqing/experiments/vqa/pruning_{}_{}/{}'
+job_cmd = "oscar/run_vqa_pruning.py \
+        --do_train --do_lower_case \
+        --model_name_or_path=experiments/vqa/Oscar_B+pruning_coef/checkpoint-0-1000 \
+        --evaluate_during_training --save_steps 1000 \
+        --self_slimming --inter_slimming --prune_before_train \
+        --pruning_strategy {} --pruning_ratio {} --seed {}\
+"
+n_repeat = 3
+for seed in range(n_repeat):
+    for pruning_ratio in [0.2, 0.4, 0.6, 0.8]:
+        for pruning_strategy in ['small', 'large', 'random']:
+            resolved_output_dir = output_dir.format(pruning_strategy, pruning_ratio, seed)
+            resolved_job_cmd = job_cmd.format(pruning_strategy, pruning_ratio, seed)
+            resolved_submit_cmd = submit_cmd.format(resolved_output_dir, task, ws_config, resolved_job_cmd)
+            print(resolved_submit_cmd)
+            os.system(resolved_submit_cmd)
+            # exit()
+            
