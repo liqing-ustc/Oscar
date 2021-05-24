df3ed9c6e8b230af67550a7652cc07c4b3f33f78
Qing Li <dylan.liqing@gmail.com>
2021-05-24 15:04:43 -0700

    Update

--- aml_setup.sh
+++ aml_setup.sh
@@ -3,6 +3,7 @@ ln -s $AML_JOB_INPUT_PATH/t-lqing/datasets
 ln -s $AML_JOB_INPUT_PATH/t-lqing/models
 ln -s $AML_JOB_INPUT_PATH/t-lqing/coco_caption
 ln -s $AML_JOB_INPUT_PATH/t-lqing/experiments
+ln -s $AML_JOB_INPUT_PATH/t-lqing data
 ln -s $AML_JOB_OUTPUT_PATH output
 df -h
 ls -al
