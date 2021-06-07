8d7aec22a3cd48c5d6400d3c1df49b2d6c933f8f
Qing Li <dylan.liqing@gmail.com>
2021-06-07 11:15:04 -0700

    Update

--- aml_setup.sh
+++ aml_setup.sh
@@ -7,5 +7,5 @@ ln -s $AML_JOB_INPUT_PATH/t-lqing data
 ln -s $AML_JOB_OUTPUT_PATH output
 df -h
 ls -al
-# export NCCL_DEBUG_SUBSYS=COLL
-# export NCCL_DEBUG=INFO
+export NCCL_DEBUG_SUBSYS=COLL
+export NCCL_DEBUG=INFO
